1d21ca2ecb1eca0cb6392c5971e1510d861ca94c
daniel <dpillay03@gmail.com>
2017-09-19 16:24:13 -0700

    done

--- daniel-cody/server.js
+++ daniel-cody/server.js
@@ -14,10 +14,10 @@ const PORT = process.env.PORT || 3000;
 // DONE: Include all of the static resources as an argument to app.use()
 APP.use(EXPRESS.static('public'));
 
-// TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
+// DONE: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
-app.post('/articles', bodyParser, function(request, response) {
+APP.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
@@ -25,7 +25,7 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
-app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+APP.listen(PORT, function() {
+  // DONE: Log to the console a message that lets you know which port your server has started on
   console.log(`Express server currently running on port ${PORT}`);
 });
