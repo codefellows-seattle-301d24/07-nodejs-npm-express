0c8237dd8bb2d5ad538e941b08f8ce8a0dd84bcd
Rob Bronson <robert.j.bronson@gmail.com>
2017-09-19 16:28:34 -0700

    added static resources with ADD.use()

--- kinley-rob/server.js
+++ kinley-rob/server.js
@@ -2,19 +2,21 @@
 
 // DONE: Initialize your project using NPM to create and populate a package.json file
 
-// DONE: Require the Express package that you installed via NPM, and instantiate the app
+// DONE: Require the Express package that you installed via NPM, and instantiate the APP
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
+const EXPRESS = require('express');
 const bodyParser = require('body-parser').urlencoded({extended: true});
+const APP = EXPRESS();
 const PORT = process.env.PORT || 3000;
 
 // TODO: Include all of the static resources as an argument to app.use()
-
+APP.use(EXPRESS.static('public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
-app.post('/articles', bodyParser, function(request, response) {
+APP.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
@@ -22,6 +24,6 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
-app.listen(PORT, function() {
+APP.listen(PORT, function() {
   // TODO: Log to the console a message that lets you know which port your server has started on
 });
