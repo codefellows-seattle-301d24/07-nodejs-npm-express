78127aa1aa535110c0094f79bcd54e864429a79f
Marco Zangari <mjzangari@gmail.com>
2017-09-19 18:46:56 -0700

    got the stretch and the last TODO, troubleshot the nodemon server

--- george-marco/starter-code/server.js
+++ george-marco/starter-code/server.js
@@ -1,24 +1,24 @@
 'use strict';
 
 // DONE: Initialize your project using NPM to create and populate a package.json file
-const APP = EXPRESS();
 
 // DONE: Require the Express package that you installed via NPM, and instantiate the app
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
 // Be sure to install that and save it as a dependency after you create your package.json.
 const EXPRESS = require('express');
+const APP = EXPRESS();
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
 // DONE: Include all of the static resources as an argument to app.use()
 APP.use(EXPRESS.static('public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
-APP.get('/', function (request, response) {
+APP.get('/article', function (request, response) {
   response.sendFile('new.html', {root: './public'});
 });
 
-app.post('/articles', bodyParser, function(request, response) {
+APP.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
@@ -26,5 +26,7 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
-app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+APP.listen(PORT, function() {
+  // DONE: Log to the console a message that lets you know which port your server has started on
+  console.log(`Express server is at this very very very moment running on port ${PORT}`);
+});
