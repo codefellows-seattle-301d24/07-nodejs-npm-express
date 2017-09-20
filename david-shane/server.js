e04391cab90275eea3c29353e8ed0654211b8bfe
shaynereedsmith <shaynereedsmith@yahoo.com>
2017-09-20 10:17:41 -0700

    Finished all remaining TODOs, logged PORT in the console and all is running correctly.

--- david-shane/server.js
+++ david-shane/server.js
@@ -2,9 +2,9 @@
 
 // DONE: Initialize your project using NPM to create and populate a package.json file
 
-// TODO: Require the Express package that you installed via NPM, and instantiate the app
+// TODO DONE: Require the Express package that you installed via NPM, and instantiate the app
 const EXPRESS = require('express');
-const PORT = process.env.PORT || 3000;
+const APP = EXPRESS();
 
 // Remember to install express, and be sure that it's been added to your package.json as a dependency
 // There is also a package here called body-parser, which is required in for use in a new route.
@@ -12,12 +12,12 @@ const PORT = process.env.PORT || 3000;
 const bodyParser = require('body-parser').urlencoded({extended: true});
 const PORT = process.env.PORT || 3000;
 
-// TODO: Include all of the static resources as an argument to app.use()
+// TODO DONE: Include all of the static resources as an argument to app.use()
 APP.use(EXPRESS.static('public'));
 // TODO: (STRETCH) Write a new route that will handle a request and send the new.html file back to the user
 
 
-app.post('/articles', bodyParser, function(request, response) {
+APP.post('/articles', bodyParser, function(request, response) {
   // REVIEW: This route will receive a new article from the form page, new.html,
   // and log that form data to the console. We will wire this up soon to actually
   // write a record to our persistence layer!
@@ -25,35 +25,7 @@ app.post('/articles', bodyParser, function(request, response) {
   response.send('Record posted to server!!');
 })
 
-app.listen(PORT, function() {
-  // TODO: Log to the console a message that lets you know which port your server has started on
+APP.listen(PORT, function() {
+  console.log(PORT);
+  // TODO DONE: Log to the console a message that lets you know which port your server has started on
 });
-
-/*
-'use strict';
-
-const EXPRESS = require('express');
-const APP = EXPRESS();
-const PORT = process.env.PORT || 3000;
-
-APP.use(EXPRESS.static('public'));
-
-APP.get('/', function(request, response){
-  response.sendFile('index.html', {root: './public'});
-});
-
-let someData = {one: 1, two: 2};
-
-APP.get('/data', function(request, response){
-  response.send(someData)
-});
-
-APP.get('*', function(request, response){
-  response.sendFile('404.html', {root: './public'});
-});
-
-APP.listen(PORT, function(){
-  console.log(`Express server currently running on port ${PORT}`);
-});
-
-*/
